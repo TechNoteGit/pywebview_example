71ac8676e260fda6bc15b65e1ebb35992c84dfb7
TechNote <technote.git@gmail.com>
2021-09-12 21:06:05 +0900

    hide window in on_closing routine

--- basic.py
+++ basic.py
@@ -1,30 +1,40 @@
 import os
 import webview
 import pystray
-import time
-import multiprocessing
+import conn
 from pystray import MenuItem
 from pystray import Menu
 from multiprocessing import Process, Pipe
 from PIL import Image
 
 
-def webview_subprocess(child_pipe):
+def webview_subprocess(conn_parent, conn_child):
     window = webview.create_window('TechNote', 'https://technote.kr')
-    webview.start(cmd_recv, [child_pipe], gui='cef', debug=True)
+    window.closing += on_closing
+    conn.parent = conn_parent
+    conn.child = conn_child
+    webview.start(cmd_recv, [], gui='cef', debug=True)
 
 
-def cmd_recv(child_pipe):
+def on_closing():
+    send_cmd_to_window('hide')
+    return False
+# https://github.com/r0x0r/pywebview/issues/573
+
+
+def cmd_recv():
     while True:
-        cmd = child_pipe.recv()
+        cmd = conn.child.recv()
         if cmd == 'show':
+            print('Show - pywebview')
             webview.windows[0].show()
         elif cmd == 'hide':
+            print('Hide - pywebview')
             webview.windows[0].hide()
 
 
-def send_cmd_to_window(parent_pipe, cmd):
-    parent_pipe.send(cmd)
+def send_cmd_to_window(cmd):
+    conn.parent.send(cmd)
 
 
 def quit_window(subprocess_handler):
@@ -33,16 +43,16 @@ def quit_window(subprocess_handler):
 
 
 if __name__ == '__main__':
-    parent_pipe, child_pipe = Pipe()
+    conn.parent, conn.child = Pipe()
 
-    subprocess_handler = Process(target=webview_subprocess, args=(child_pipe,))
+    subprocess_handler = Process(target=webview_subprocess, args=(conn.parent, conn.child))
     subprocess_handler.start()
 
     # Using window tray
     base_path = os.path.dirname(os.path.abspath(__file__))
     image_path = Image.open(base_path + '/res/tray_icon.png')
-    menu = Menu(MenuItem('Hide', lambda: send_cmd_to_window(parent_pipe, 'hide')),
-                MenuItem('Show', lambda: send_cmd_to_window(parent_pipe, 'show')),
+    menu = Menu(MenuItem('Hide', lambda: send_cmd_to_window('hide')),
+                MenuItem('Show', lambda: send_cmd_to_window('show')),
                 MenuItem('Quit', lambda: quit_window(subprocess_handler)))
     icon = pystray.Icon('pyWebView_Sample', image_path, 'pyWebView', menu)
     icon.run()
---
+++ conn.py
@@ -0,0 +1,2 @@
+parent = None
+child = None
